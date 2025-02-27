60338fd2a8b46a41670c504fc54f4290f23b7bcd
Shizer <ven.rocket@gmail.com>
2025-02-27 18:49:10 +0100

    Update Config_template.bat
    Add copyfolder Result

--- files/Config_template.bat
+++ files/Config_template.bat
@@ -3,7 +3,7 @@ SET origin=
 SET destination=
 
 SET origin_dir=/cygdrive/C/FolderToCopy
-SET origin_local_copy_dir=/cygdrive/C/CopyFolder
+SET origin_local_copy_dir=/cygdrive/C/CopyFolder/FolderToCopy
 SET destination_address=username@domain::Folder/%origin%
 
 SET exclude={"DataToExclude/TMP/*"}
